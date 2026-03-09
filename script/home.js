af84af08e72fec79215cf49a5af7f8fd2e96b382
muradvc <muradvcv@gmail.com>
2026-03-09 15:03:25 +0600

    tab btn maked

--- script/home.js
+++ script/home.js
@@ -65,3 +65,107 @@ const displayIssueDetails = (issue) => {
   document.getElementById("issue_modal").showModal();
 };
 
+
+// card load
+
+function showIssues(issues) {
+  const issuesContainer = document.getElementById("issues-container");
+  issuesContainer.innerHTML = "";
+
+  // filter issue
+  let filteredIssues;
+
+  if (activeTab === "all") {
+    filteredIssues = issues;
+  } else {
+    filteredIssues = issues.filter(function (issue) {
+      return issue.status === activeTab;
+    });
+  }
+
+  const issueCount = document.getElementById("issue-count");
+  issueCount.innerText = `${filteredIssues.length} Issues`;
+
+  for (let issue of issues) {
+    // console.log(issue)
+    const statusImg = issue.status === "open"
+      ? "assets/Open-Status.png"
+      : "assets/Closed-Status.png";
+
+    const issueCard = document.createElement("div");
+
+    // add border top dynamically
+    issueCard.style.borderTop = issue.status === "open" ? "4px solid #00A96E" : "4px solid #A855F7";
+    issueCard.style.borderRadius = "0.5rem";
+
+    // labels dynamically
+    const labelAdd = issue.labels.map(label => `
+                <button class="btn btn-soft btn-secondary rounded-full border">${label.toUpperCase()}</button>
+            `).join(" ")
+
+
+    issueCard.innerHTML = `
+                <div class="issue-card space-y-4 shadow-sm p-4 rounded-lg">
+
+                    <!-- card status img -->
+                    <div class="card-img-priority flex flex-1 justify-between items-center">
+                        <div>
+                            <img width="30px" src="${statusImg}" alt="">
+                        </div>
+                        
+                        <button class="btn btn-soft btn-secondary rounded-full">${issue.priority}</button>
+                    </div>
+
+                    <!-- card header -->
+                     <div class="card-header space-y-2">
+                        <h2 class="font-semibold">${issue.title}</h2>
+                        <p class="text-[#64748B] text-[14px]">${issue.description}</p>
+                     </div>
+
+                     <!-- topics name -->
+                      ${labelAdd}
+
+                      <!-- date -->
+
+                      <div class="date py-4 border-t border-[#64748B]">
+                        <p class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+                        <p class="text-[#64748B]">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                      </div>
+
+
+                </div>
+        `
+
+    issueCard.addEventListener("click", () => loadIssueDetail(issue.id));
+    issuesContainer.append(issueCard);
+
+  }
+}
+
+// tab-buttons
+const tabButtons = document.querySelectorAll(".issue-tab-btn");
+tabButtons.forEach(btn => {
+  btn.addEventListener("click", async () => {
+    activeTab = btn.dataset.tab;
+
+    tabButtons.forEach(b => {
+      b.classList.remove("btn-primary");
+      b.classList.add("btn-outline");
+    });
+
+    btn.classList.remove("btn-outline");
+    btn.classList.add("btn-primary");
+
+    manageSpinner(true);
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    if (activeTab === "all") {
+      showIssues(issuesData);
+    } else {
+      const filtered = issuesData.filter(issue => issue.status === activeTab);
+      showIssues(filtered);
+    }
+    manageSpinner(false);
+  })
+})
+
